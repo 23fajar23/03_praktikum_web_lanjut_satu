9746a0840961f4b10313d268c7383c5b0cfd24fd
= <=>
2021-02-27 18:48:34 +0700

    Pembuatan controller dan web view telah selesai (Praktikum 1 Success)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\ProgramController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +20,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route 
+Route::get('/',[HomeController::class,'index']);
+
+// Route Prefix
+Route::prefix('product')->group(function () {
+    Route::get('1',[ProductsController::class,'product']);
+    
+});
+
+//Route Parameter
+Route::get('/news/{id}', function ($id) {
+    return 'asds';
+    
+    // Route::get('/news',[NewsController::class,'news']);
+});
+    
+// Route Prefix
+Route::prefix('program')->group(function () {
+    Route::get('1',[ProgramController::class,'program']);    
+});
+
+// Route Public
+Route::get('/about',[AboutController::class,'about']);
+
+// Route Resource
+Route::get('/contact',[ContactController::class,'index']);
